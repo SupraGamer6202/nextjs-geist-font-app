893e160ab06da23aed6f36c572bf790da99077aa
SupraGamer6202 <129367971+SupraGamer6202@users.noreply.github.com>
2025-08-09 12:51:11 +0200

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754736671116

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_711ffab9._.js",
+  "static/chunks/node_modules_9a8549d4._.js"
+],
+    source: "dynamic"
+});
